792adbe60a52649efc27ee0415e1d86f31f9eddf
Suraj Kumar <suraj.20227053@mnnit.ac.in>
2025-08-04 15:23:26 +0530

    final deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,21 +6,16 @@ import notesRoutes from "./routes/notesRoutes.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 import authRoutes from './routes/auth.js';
 import path from "path";
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve();
 
-
 // Global Middleware
-if (process.env.NODE_ENV !== "production") {
-  app.use(
-    cors({
-      origin: "http://localhost:5173",
-    })
-  );
-}
+// The cors middleware is applied unconditionally.
+app.use(cors());
 
 app.use(express.json());
 app.use(rateLimiter);
@@ -29,12 +24,12 @@ app.use(rateLimiter);
 app.use("/api/notes", notesRoutes);
 app.use('/api/auth', authRoutes);
 
-if(process.env.NODE_ENV === "production"){
-  app.use(express.static(path.join(__dirname,"../frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*",(req,res) =>{
-    res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
-  })
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
 
 app.get("/", (req, res) => res.send("API Running"));
